9acc7ecaf2eef99bdfcd67e95ce6376265661931
Georgi Inkov <georgi.inkov@scaleforce.io>
2025-07-08 17:29:51 +0300

    Remove projects

--- 03.Raiding/Core/Interfaces/IEngine.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Raiding.Core.Interfaces;
-public interface IEngine
-{
-    void Run();
-}
--- 03.Raiding/Factory/Interfaces/IHeroFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿using Raiding.Models.Interfaces;
-
-namespace Raiding.Factory.Interfaces;
-public interface IHeroFactory
-{
-    IHero Create(string type, string name);
-}
